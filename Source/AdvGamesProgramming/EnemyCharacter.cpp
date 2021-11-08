9e3b06afe4b1e05b5053c18055ada1155897a7c9
Ash <geoffwu1@hotmail.com>
2021-11-08 19:45:17 +1100

    PLEASE DONT DIE

--- Source/AdvGamesProgramming/EnemyCharacter.cpp
+++ Source/AdvGamesProgramming/EnemyCharacter.cpp
@@ -1,59 +1,308 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "HealthComponent.h"
+#include "EnemyCharacter.h"
+#include "AIManager.h"
+#include "NavigationNode.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Perception/AIPerceptionComponent.h"
 
-// Sets default values for this component's properties
-UHealthComponent::UHealthComponent()
+// Sets default values
+AEnemyCharacter::AEnemyCharacter()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-	MaxHealth = 100.0f;
-	// ...
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CurrentAgentState = AgentState::PATROL;
+	PathfindingDestinationAccuracy = 100.0f;
+	PathfindingNodeAccuracy = PathfindingDestinationAccuracy;
+
+	Superior = nullptr;
+	Inferior = nullptr;
 }
 
+AEnemyCharacter::AEnemyCharacter(AEnemyCharacter* SuperiorAI)
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
 
-// Called when the game starts
-void UHealthComponent::BeginPlay()
+	CurrentAgentState = AgentState::PATROL;
+	PathfindingDestinationAccuracy = 100.0f;
+	PathfindingNodeAccuracy = PathfindingDestinationAccuracy;
+
+	Superior = SuperiorAI;
+}
+
+// Called when the game starts or when spawned
+void AEnemyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	CurrentHealth = MaxHealth;
+	Cast<UCharacterMovementComponent>(GetMovementComponent())->bOrientRotationToMovement = true;
+
+	HealthComponent = FindComponentByClass<UHealthComponent>();
+
+	PerceptionComponent = FindComponentByClass<UAIPerceptionComponent>();
+	if (PerceptionComponent)
+	{
+		PerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &AEnemyCharacter::SensePlayer);
+	}
+
+	DetectedActor = nullptr;
+	bCanSeeActor = false;
+	TravelTimer = MaxTimeBeforeReroute;
+
+	PathfindingNodeAccuracy = PathfindingDestinationAccuracy;
 
 }
-
 
 // Called every frame
-void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void AEnemyCharacter::Tick(float DeltaTime)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	Super::Tick(DeltaTime);
+	if (Superior && HealthComponent->CurrentHealth > 40.0f)
+	{
+		if (bCanSeeActor && !Superior->bCanSeeActor)
+		{
+			Superior->DetectedActor = DetectedActor;
+			Superior->Path.Empty();
+			Superior->AgentEngage();
+			Superior->CurrentAgentState = AgentState::ENGAGE;
+
+			Path.Empty();
+			AgentEngage();
+
+			UE_LOG(LogTemp, Warning, TEXT("PLAYER SPOTTED"));
+		}
+
+		CurrentAgentState = Superior->CurrentAgentState;
+		DetectedActor = Superior->DetectedActor;
+	}
+
+	FindPath();
+
+
+	if (HealthComponent->CurrentHealth == 0)
+	{
+		if (Inferior)Inferior->Superior = Superior;
+		if (Superior)Superior->Inferior = Inferior;
+		Destroy();
+	}
+
+	if (TravelTimer > 0) TravelTimer -= DeltaTime;
+
+	if (LastNode)
+	{
+		if ((GetActorLocation() - LastNode->GetActorLocation()).SizeSquared() > 100)
+		{
+			LastNode->bIsOccupied = false;
+		}
+	}
+
+	MoveAlongPath();
 }
 
-void UHealthComponent::OnTakeDamage(float Damage)
+// Called to bind functionality to input
+void AEnemyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	CurrentHealth -= Damage;
-	if (CurrentHealth < 0.0f)
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AEnemyCharacter::AgentPatrol()
+{
+	if (Path.Num() == 0)
 	{
-		CurrentHealth = 0;
-		OnDeath();
+		if (Superior && Manager)
+		{
+			Path = Manager->GeneratePath(CurrentNode, Superior->CurrentNode);
+		}
+		else if (Manager)
+		{
+			Path = Manager->GeneratePath(CurrentNode, Manager->MainNodes[FMath::RandRange(0, Manager->MainNodes.Num() - 1)]);
+		}
 	}
 }
 
-void UHealthComponent::OnDeath()
+void AEnemyCharacter::AgentEngage()
 {
+	if (bCanSeeActor && DetectedActor)
+	{
+		FVector FireDirection = DetectedActor->GetActorLocation() - GetActorLocation();
+		Fire(FireDirection);
+	}
+	if (Path.Num() == 0 && DetectedActor)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("ENGAGING PLAYER"));
 
+		ANavigationNode* NearestNode = Manager->FindNearestNode(DetectedActor->GetActorLocation());
+		if (NearestNode != CurrentNode && !(CurrentNode->ConnectedNodes.Contains(NearestNode) && NearestNode->bIsOccupied))
+		{
+			Path = Manager->GeneratePath(CurrentNode, NearestNode);
+		}
+
+	}
 }
 
-float UHealthComponent::HealthPercentageRemaining()
+void AEnemyCharacter::AgentEvade()
 {
-	return CurrentHealth / MaxHealth * 100.0f;
+	if (bCanSeeActor && DetectedActor)
+	{
+		FVector FireDirection = DetectedActor->GetActorLocation() - GetActorLocation();
+		Fire(FireDirection);
+	}
+	if (Path.Num() == 0 && DetectedActor)
+	{
+
+		ANavigationNode* FurthestNode = Manager->FindFurthestNode(DetectedActor->GetActorLocation());
+
+		if (FurthestNode != CurrentNode && !(CurrentNode->ConnectedNodes.Contains(FurthestNode) && FurthestNode->bIsOccupied))
+		{
+			Path = Manager->GeneratePath(CurrentNode, FurthestNode);
+		}
+	}
 }
 
-void UHealthComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+void AEnemyCharacter::SensePlayer(AActor* ActorSensed, FAIStimulus Stimulus)
 {
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	if (ActorSensed->ActorHasTag(FName(TEXT("Player"))))
+	{
+		if (Stimulus.WasSuccessfullySensed())
+		{
 
-	DOREPLIFETIME(UHealthComponent, CurrentHealth);
-
+			UE_LOG(LogTemp, Warning, TEXT("Player Detected"))
+				DetectedActor = ActorSensed;
+			bCanSeeActor = true;
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Player Lost"))
+				bCanSeeActor = false;
+		}
+	}
 }
+
+void AEnemyCharacter::MoveAlongPath()
+{
+	if (!(GetLocalRole() == ENetRole::ROLE_Authority)) return;
+	if ((GetActorLocation() - CurrentNode->GetActorLocation()).IsNearlyZero(PathfindingNodeAccuracy)
+		&& Path.Num() > 0)
+	{
+		TravelTimer = MaxTimeBeforeReroute;
+		LastNode = CurrentNode;
+		CurrentNode = Path.Pop();
+		ANavigationNode* NextNode = CurrentNode;
+		bIsStuck = false;
+
+		if (Path.Num() == 0 && CurrentNode->bIsOccupied)
+		{
+			int8 i = 5;
+			while (NextNode->bIsOccupied && i >= 5)
+			{
+				NextNode = NextNode->ConnectedNodes[FMath::RandRange(0, NextNode->ConnectedNodes.Num() - 1)];
+				i--;
+			}
+
+			Path.Empty();
+			Path = Manager->GeneratePath(LastNode, NextNode);
+			CurrentNode = LastNode;
+			PathfindingNodeAccuracy = PathfindingEvasionAccuracy;
+
+			UE_LOG(LogTemp, Error, TEXT("%s Node: %s is Occupied. Moving to %s"), *GetName(), *CurrentNode->GetName(), *NextNode->GetName());
+
+		}
+
+		if (Path.Num() == 0)
+		{
+			CurrentNode->bIsOccupied = true;
+			UE_LOG(LogTemp, Error, TEXT("%s Node: %s has been Occupied"), *GetName(), *CurrentNode->GetName());
+			PathfindingNodeAccuracy = PathfindingDestinationAccuracy;
+		}
+
+	}
+	else if (!(GetActorLocation() - CurrentNode->GetActorLocation()).IsNearlyZero(PathfindingNodeAccuracy))
+	{
+		AddMovementInput(CurrentNode->GetActorLocation() - GetActorLocation());
+		if (TravelTimer <= 0)
+		{
+			TravelTimer = MaxTimeBeforeReroute;
+			Path.Insert(CurrentNode, 0);
+			if (!LastNode)
+			{
+				CurrentNode = Manager->FindNearestNode(GetActorLocation());
+				FindPath();
+
+			}
+			else if (LastNode->bIsOccupied)
+			{
+				CurrentNode = Manager->FindNearestNode(GetActorLocation());
+				FindPath();
+			}
+			else if (bIsStuck)
+			{
+				CurrentNode = Manager->FindNearestNode(GetActorLocation());
+				FindPath();
+			}
+			else
+			{
+				CurrentNode = LastNode;
+				FindPath();
+			}
+
+			bIsStuck = true;
+
+		}
+	}
+
+	/*if (Path.Num() == 1 && CurrentNode->bIsOccupied)
+	{
+		Path.Add(CurrentNode->ConnectedNodes[FMath::RandRange(0, CurrentNode->ConnectedNodes.Num() - 1)]);
+		CurrentNode = Path.Pop();
+	}*/
+}
+
+void  AEnemyCharacter::FindPath()
+{
+	if (!(GetLocalRole() == ENetRole::ROLE_Authority)) return;
+	if (CurrentAgentState == AgentState::PATROL)
+	{
+		AgentPatrol();
+		if (bCanSeeActor && HealthComponent->HealthPercentageRemaining() >= 40.0f)
+		{
+			CurrentAgentState = AgentState::ENGAGE;
+			Path.Empty();
+		}
+		else if (bCanSeeActor && HealthComponent->HealthPercentageRemaining() < 40.0f)
+		{
+			CurrentAgentState = AgentState::EVADE;
+			Path.Empty();
+		}
+	}
+	else if (CurrentAgentState == AgentState::ENGAGE)
+	{
+		AgentEngage();
+		if (!bCanSeeActor)
+		{
+			CurrentAgentState = AgentState::PATROL;
+		}
+		else if (bCanSeeActor && HealthComponent->HealthPercentageRemaining() < 40.0f)
+		{
+			CurrentAgentState = AgentState::EVADE;
+			Path.Empty();
+		}
+	}
+	else if (CurrentAgentState == AgentState::EVADE)
+	{
+		AgentEvade();
+		if (!bCanSeeActor)
+		{
+			CurrentAgentState = AgentState::PATROL;
+		}
+		else if (bCanSeeActor && HealthComponent->HealthPercentageRemaining() >= 40.0f)
+		{
+			CurrentAgentState = AgentState::ENGAGE;
+			Path.Empty();
+		}
+	}
+}
+
